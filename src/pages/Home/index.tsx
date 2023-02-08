a712665ba268e9a14af1b446e6146926b6cf9e4f
szheron <heronbr7@gmail.com>
2023-02-08 16:16:51 -0300

    fix: adicionado resticao ao criar posts para usuarios sem autenticacao

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -61,7 +61,7 @@ export default function Home(){
         return (
             <Container>
                 {
-                    !user?(
+                    !user.id?(
                         <Profile user={null}/>
                     ):(
                         <>
