eb2501385094097c3f7c901251238c935792f9d5
szheron <heronbr7@gmail.com>
2023-02-11 15:48:38 -0300

    feat: posts ordenados por data

--- src/utils/getPosts.ts
+++ src/utils/getPosts.ts
@@ -1,9 +1,9 @@
-import { query, collection, getDocs } from "firebase/firestore"
+import { query, collection, getDocs, orderBy } from "firebase/firestore"
 import { db } from "../services/firebase";
 import { PostData } from "../pages/Home";
 
 export async function getPosts() {
-    const q = query(collection(db, "posts"));
+    const q = query(collection(db, "posts"), orderBy("createdAt", "desc"));
     const querySnapshot = await getDocs(q);
     let tempPosts: Array<PostData> = []
 
